8b2f01f125cf08b0b06c7bcd54755b401e9f30af
NMinhNam <mhnamm10@gmail.com>
2026-04-22 08:44:04 +0700

    add /healthz

--- src/main/java/com/erp/bom/feature/common/controller/HealthController.java
+++ src/main/java/com/erp/bom/feature/common/controller/HealthController.java
@@ -3,13 +3,11 @@ package com.erp.bom.feature.common.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api")
 public class HealthController {
 
     @GetMapping("/healthz")
